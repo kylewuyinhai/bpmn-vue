0ee6ac48f1985c561ca19f2d055c6095ba8ca2ca
kyle <742973654@qq.com>
2020-03-29 14:02:54 +0800

    bpmb-panel

--- src/components/bpmn/vBpmn.vue
+++ src/components/bpmn/vBpmn.vue
@@ -15,10 +15,14 @@
 
 <script>
 // import BpmnModeler from "bpmn-js/lib/Modeler";
-// import propertiesPanelModule from "bpmn-js-properties-panel";
+import propertiesPanelModule from "bpmn-js-properties-panel";
+// 自定义的 properties-panel内容
+import propertiesProviderModule from "./CustomPanel/provider/authority";
+// 引入描述文件
+import authorityModdleDescriptor from "./CustomPanel/descriptors/authority";
 // import propertiesProviderModule from "bpmn-js-properties-panel/lib/provider/camunda";
 import customTranslate from "./CustomTranslate"; //汉化引入
-import customModeler from './customModeler'
+import customModeler from "./customModeler";
 import { xmlStr } from "./mock";
 export default {
   name: "vBpmn",
@@ -32,8 +36,8 @@ export default {
   },
   created() {},
   mounted() {
-		this.initBpmn()
-	},
+    this.initBpmn();
+  },
   methods: {
     initBpmn() {
       const canvas = this.$refs.canvas;
@@ -42,37 +46,39 @@ export default {
         //添加控制板
         propertiesPanel: {
           parent: "#js-properties-panel"
-				},
-				//汉化
-				
+        },
+        //汉化
+
         additionalModules: [
           // 左边工具栏以及节点
-          // propertiesProviderModule,
-					// propertiesPanelModule
-					{translate: ['value', customTranslate]}
-        ]
-			})
-			this.bpmnModeler.importXML(xmlStr, err => {
+          propertiesProviderModule,
+          propertiesPanelModule,
+          { translate: ["value", customTranslate] }
+        ],
+        moddleExtensions: {
+          // camunda: camundaModdleDescriptor,
+          authority: authorityModdleDescriptor
+        }
+      });
+      this.bpmnModeler.importXML(xmlStr, err => {
         if (err) {
-          console.error(err)
+          console.error(err);
         } else {
-          this.success()
+          this.success();
         }
         // 让图能自适应屏幕
-        var canvas = this.bpmnModeler.get('canvas')
-        canvas.zoom('fit-viewport')
-      })
-		},
-		success() {
-			console.log('创建成功');
-			
-		}
+        var canvas = this.bpmnModeler.get("canvas");
+        canvas.zoom("fit-viewport");
+      });
+    },
+    success() {
+      console.log("创建成功");
+    }
   }
 };
 </script>
 
 <style>
-
 .v-bpmn {
   background-color: #ffffff;
   width: 100%;
@@ -110,7 +116,7 @@ export default {
   background: #fff;
   cursor: pointer;
 }
-.bjs-powered-by{
-	display: none;
+.bjs-powered-by {
+  display: none;
 }
 </style>
